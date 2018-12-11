fab66e914d6d50f618efe2e8a2e1404b123041ec
Polymorph <noonphongnarin@gmail.com>
2018-12-11 13:10:32 +0700

    fix main

--- main.go
+++ main.go
@@ -39,8 +39,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowOrigins: []string{"*"}
 	}))
 
 	// Login route
